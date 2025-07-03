a7682d2fa2906db77da9923caf3890c252722d02
Huy Nguyen <hueezer@gmail.com>
2025-07-03 10:50:41 -0700

    Initial Commit

---
+++ HuysEnPlace/HuysEnPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuysEnPlaceApp.swift
+//  HuysEnPlace
+//
+//  Created by Huy Nguyen on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HuysEnPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
